7bf39180e20c01f175cd2570e94e9e94d2fcd1c3
오준호 <ojh0050118@gmail.com>
2022-04-06 00:50:07 +0900

    로거 추가

---
+++ AdofaiToCircle/Logging/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace AdofaiToCircle.Logging
+{
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Error(Exception e, string description)
+        {
+            Console.WriteLine(e.Message + description);
+        }
+    }
+}
